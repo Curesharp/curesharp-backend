1604814d102185f6e1fffd0cc3ec1ac53ca31682
Kayque Nunes <kayque.nunes@portoseguro.com.br>
2023-11-20 23:46:08 -0300

    fix: cors

--- src/main/java/com/curesharp/config/cors/CorsFilter.java
+++ src/main/java/com/curesharp/config/cors/CorsFilter.java
@@ -1,17 +1,33 @@
 package com.curesharp.config.cors;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.ext.Provider;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@Provider
-public class CorsFilter implements ContainerResponseFilter {
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) res;
+
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        chain.doFilter(req, res);
     }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {    }
+
+    @Override
+    public void destroy() {    }
 }
